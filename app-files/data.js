f6405c9a7aaf443511d0e8c2df5dfe3605d9f9ff
BahamutChaos666 <aosscorpio3@gmail.com>
2025-08-07 18:47:21 +0200

    Ajout des fichiers Marzipano
    Ajout des fichiers Marzipano

---
+++ app-files/data.js
@@ -0,0 +1,104 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-essaie_1_roses",
+      "name": "Essaie_1_roses",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2918453010043205,
+          "pitch": -0.050431883613761386,
+          "rotation": 6.283185307179586,
+          "target": "0-essaie_1_roses"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.1792344186463453,
+          "pitch": 0.2993412602423611,
+          "title": "<strong data-start=\"145\" data-end=\"178\">Tomate cerise</strong><br data-start=\"178\" data-end=\"181\">\n<em data-start=\"184\" data-end=\"206\">Solanum lycopersicum</em>",
+          "text": "<p data-start=\"154\" data-end=\"399\"><strong data-start=\"186\" data-end=\"205\"><em data-start=\"188\" data-end=\"203\">Description :</em></strong><br data-start=\"205\" data-end=\"208\">\n<strong data-start=\"208\" data-end=\"237\">Petite, juteuse et sucrée</strong>, la tomate cerise est une star des apéros d'été. Elle pousse en grappes sur un plant touffu et aime le soleil. <strong data-start=\"349\" data-end=\"399\">À cueillir rouge vif pour un maximum de goût !</strong></p><p data-start=\"154\" data-end=\"399\"><strong data-start=\"349\" data-end=\"399\"><br></strong></p><p data-start=\"154\" data-end=\"399\">\n</p><p data-start=\"401\" data-end=\"577\"><strong data-start=\"401\" data-end=\"426\"><em data-start=\"403\" data-end=\"424\">Conseil jardinage :</em></strong>&nbsp;</p><p data-start=\"401\" data-end=\"577\"><strong data-start=\"427\" data-end=\"451\">Arrose régulièrement</strong>, sans mouiller les feuilles. Un tuteur l’aide à grimper.</p><p data-start=\"401\" data-end=\"577\"><br data-start=\"508\" data-end=\"511\">\n<strong data-start=\"511\" data-end=\"527\"><em data-start=\"513\" data-end=\"525\">Anecdote :</em></strong>&nbsp;</p><p data-start=\"401\" data-end=\"577\"><strong data-start=\"528\" data-end=\"574\">Saviez-vous qu’elle est en fait un fruit ?</strong> 🍅</p>"
+        },
+        {
+          "yaw": -1.3870585493106073,
+          "pitch": 0.22777829305497832,
+          "title": "<strong data-start=\"232\" data-end=\"270\">Tomate cerise noire</strong><div><em data-start=\"276\" data-end=\"298\">Solanum lycopersicum</em></div>",
+          "text": "<p data-start=\"198\" data-end=\"456\"><strong data-start=\"236\" data-end=\"255\"><em data-start=\"238\" data-end=\"253\">Description :</em></strong><br data-start=\"255\" data-end=\"258\">\n<strong data-start=\"258\" data-end=\"285\">Originale et savoureuse</strong>, cette variété ancienne produit de petites tomates à la peau pourpre presque noire et à la chair douce et sucrée. <strong data-start=\"400\" data-end=\"456\">Idéale pour étonner dans une salade ou en apéritif !</strong></p><p data-start=\"198\" data-end=\"456\"><strong data-start=\"400\" data-end=\"456\"><br></strong></p>\n<p data-start=\"458\" data-end=\"620\"><strong data-start=\"458\" data-end=\"483\"><em data-start=\"460\" data-end=\"481\">Conseil jardinage :</em></strong>&nbsp;</p><p data-start=\"458\" data-end=\"620\"><strong data-start=\"484\" data-end=\"530\">Elle aime le soleil et un sol bien drainé.</strong> Arrosez régulièrement, sans mouiller les feuilles, et tuteurez pour soutenir les grappes.</p><p data-start=\"458\" data-end=\"620\"><br></p>\n<p data-start=\"622\" data-end=\"763\"><strong data-start=\"622\" data-end=\"638\"><em data-start=\"624\" data-end=\"636\">Anecdote :</em></strong> <strong data-start=\"639\" data-end=\"710\">Sa couleur sombre est due à une forte concentration d’anthocyanines</strong>, des antioxydants naturels bénéfiques pour la santé.</p>"
+        },
+        {
+          "yaw": -2.5727084910346356,
+          "pitch": 0.3563889893482646,
+          "title": "Framboisier (Rubus idaeus)",
+          "text": "<p data-start=\"209\" data-end=\"450\"><strong data-start=\"254\" data-end=\"273\"><em data-start=\"256\" data-end=\"271\">Description :</em></strong><br data-start=\"273\" data-end=\"276\">\n<strong data-start=\"276\" data-end=\"307\">Arbrisseau fruitier épineux</strong> qui produit de délicieuses petites baies rouges sucrées et légèrement acidulées, les framboises. Très apprécié des enfants comme des oiseaux !</p><p data-start=\"209\" data-end=\"450\"><br></p>\n<p data-start=\"452\" data-end=\"657\"><strong data-start=\"452\" data-end=\"477\"><em data-start=\"454\" data-end=\"475\">Conseil jardinage :</em></strong><br data-start=\"477\" data-end=\"480\">\n<strong data-start=\"480\" data-end=\"527\">Aime les sols frais, riches et bien drainés</strong>, de préférence à mi-ombre. <strong data-start=\"555\" data-end=\"578\">Tuteurez les cannes</strong> pour les maintenir droites. Taillez chaque année pour favoriser la production.</p>\n<p data-start=\"659\" data-end=\"834\"><strong data-start=\"659\" data-end=\"675\"><em data-start=\"661\" data-end=\"673\"><br></em></strong></p><p data-start=\"659\" data-end=\"834\"><strong data-start=\"659\" data-end=\"675\"><em data-start=\"661\" data-end=\"673\">Anecdote :</em></strong><br data-start=\"675\" data-end=\"678\">\n<strong data-start=\"678\" data-end=\"733\">La framboise est en fait composée de petites drupes</strong>, chacune contenant une graine. Elle était déjà consommée à l’état sauvage à l’époque préhistorique !</p>"
+        },
+        {
+          "yaw": -0.49710554460170187,
+          "pitch": 0.05672221775494535,
+          "title": "Title",
+          "text": "<p data-start=\"204\" data-end=\"458\"><strong data-start=\"249\" data-end=\"268\"><em data-start=\"251\" data-end=\"266\">Description :</em></strong><br data-start=\"268\" data-end=\"271\">\n<strong data-start=\"271\" data-end=\"316\">Légume-fruit long et vert (parfois jaune)</strong>, à la chair tendre et au goût doux. Très productive en été, la courgette se cuisine de mille façons : sautée, farcie, en soupe ou en gâteau !</p><p data-start=\"204\" data-end=\"458\"><br></p>\n<p data-start=\"460\" data-end=\"692\"><strong data-start=\"460\" data-end=\"485\"><em data-start=\"462\" data-end=\"483\">Conseil jardinage :</em></strong><br data-start=\"485\" data-end=\"488\">\n<strong data-start=\"488\" data-end=\"546\">Elle a besoin de soleil, de chaleur et d’un sol riche.</strong> Arrosez régulièrement au pied, sans mouiller le feuillage pour éviter les maladies.<br data-start=\"630\" data-end=\"633\">\n<strong data-start=\"633\" data-end=\"656\">Prévoir de l’espace</strong>, car elle s’étale largement au sol.</p><p data-start=\"460\" data-end=\"692\"><br></p>\n<p data-start=\"694\" data-end=\"832\"><strong data-start=\"694\" data-end=\"710\"><em data-start=\"696\" data-end=\"708\">Anecdote :</em></strong><br data-start=\"710\" data-end=\"713\">\n<strong data-start=\"713\" data-end=\"757\">Les fleurs de courgette sont comestibles</strong> et délicieuses en beignets ou farcies. Un vrai délice à ne pas gaspiller !</p>"
+        },
+        {
+          "yaw": 1.1005625238628483,
+          "pitch": 0.171000412475534,
+          "title": "Rosiers",
+          "text": "<p data-start=\"210\" data-end=\"471\"><strong data-start=\"247\" data-end=\"266\"><em data-start=\"249\" data-end=\"264\">Description :</em></strong><br data-start=\"266\" data-end=\"269\">\n<strong data-start=\"269\" data-end=\"309\">Plante vivace à fleurs emblématiques</strong>, le rosier offre une grande variété de formes, de couleurs et de parfums. Il fleurit du printemps à l’automne, attirant les abeilles et enchantant les visiteurs.</p><p data-start=\"210\" data-end=\"471\"><br></p>\n<p data-start=\"473\" data-end=\"691\"><strong data-start=\"473\" data-end=\"498\"><em data-start=\"475\" data-end=\"496\">Conseil jardinage :</em></strong><br data-start=\"498\" data-end=\"501\">\n<strong data-start=\"501\" data-end=\"560\">Plantez-le au soleil, dans un sol bien drainé et riche.</strong> Taillez chaque année pour stimuler la floraison et éviter les maladies.<br data-start=\"632\" data-end=\"635\">\n<strong data-start=\"635\" data-end=\"654\">Arrosez au pied</strong> et évitez d’humidifier les feuilles.</p><p data-start=\"473\" data-end=\"691\"><br></p>\n<p data-start=\"693\" data-end=\"845\"><strong data-start=\"693\" data-end=\"709\"><em data-start=\"695\" data-end=\"707\">Anecdote :</em></strong><br data-start=\"709\" data-end=\"712\">\n<strong data-start=\"712\" data-end=\"763\">Symbole d’amour et de beauté depuis l’Antiquité</strong>, la rose est aussi utilisée en cuisine (eau de rose, confiture) et en cosmétique.</p>"
+        },
+        {
+          "yaw": -1.38049118072737,
+          "pitch": 0.5357265020792532,
+          "title": "Title",
+          "text": "<p data-start=\"188\" data-end=\"447\"><strong data-start=\"241\" data-end=\"260\"><em data-start=\"243\" data-end=\"258\">Description :</em></strong><br data-start=\"260\" data-end=\"263\">\n<strong data-start=\"263\" data-end=\"292\">Tubercule riche en amidon</strong>, la pomme de terre est un incontournable de la cuisine du monde. Elle existe en nombreuses variétés, de formes et de couleurs variées, du jaune au violet.</p><p data-start=\"188\" data-end=\"447\"><br></p>\n<p data-start=\"449\" data-end=\"691\"><strong data-start=\"449\" data-end=\"474\"><em data-start=\"451\" data-end=\"472\">Conseil jardinage :</em></strong><br data-start=\"474\" data-end=\"477\">\n<strong data-start=\"477\" data-end=\"541\">Plantez-les au printemps, dans un sol meuble et bien drainé.</strong> Hiller régulièrement pour protéger les tubercules de la lumière et éviter qu’ils verdissement.<br data-start=\"636\" data-end=\"639\">\n<strong data-start=\"639\" data-end=\"661\">Arrosez modérément</strong> et évitez l’excès d’humidité.</p><p data-start=\"449\" data-end=\"691\"><br></p>\n<p data-start=\"693\" data-end=\"822\"><strong data-start=\"693\" data-end=\"709\"><em data-start=\"695\" data-end=\"707\">Anecdote :</em></strong><br data-start=\"709\" data-end=\"712\">\n<strong data-start=\"712\" data-end=\"744\">Originaire d’Amérique du Sud</strong>, la pomme de terre a révolutionné l’alimentation en Europe dès le 18ᵉ siècle.</p>"
+        },
+        {
+          "yaw": -2.942372508851914,
+          "pitch": 0.10959775463245869,
+          "title": "Test",
+          "text": "&lt;p&gt;Donnez votre avis : &lt;a href=\"https://www.youtube.com/\" target=\"_blank\"&gt;Cliquez ici&lt;/a&gt;&lt;/p&gt;"
+        },
+        {
+          "yaw": 2.7115372101147637,
+          "pitch": -0.27597582392757936,
+          "title": "test2",
+          "text": "Text"
+        },
+        {
+          "yaw": 3.127534449905321,
+          "pitch": -0.17620282989799563,
+          "title": "test3video integration",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
